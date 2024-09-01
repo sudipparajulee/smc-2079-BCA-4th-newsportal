9bc53d35ef415cd6d70a98a28a87b9c5157a5e1e
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-09-01 06:52:32 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <nav class="flex items-center justify-between px-12">
+        <img src="https://smc.edu.np/wp-content/uploads/2023/11/smc-logo-circle.png" alt="" class="h-24">
+        <div class="flex">
+            <a href="index.php" class="p-2">Home</a>
+            <a href="" class="p-2">Sport</a>
+            <a href="" class="p-2">Politics</a>
+            <a href="" class="p-2">National</a>
+            <a href="" class="p-2">International</a>
+            <a href="" class="p-2">Login</a>
+        </div>
+    </nav>
+    <h1 class="text-center font-bold text-2xl bg-red-500 text-white p-2">This is PHP Project</h1>
+</body>
+</html>
