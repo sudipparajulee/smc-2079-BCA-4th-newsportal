fd0ebb1410f8b6a49d70bc83fbd812c85713254e
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-09-22 07:33:15 +0545

    Category

--- admin/actioncategory.php
+++ admin/actioncategory.php
@@ -4,7 +4,20 @@ if(isset($_POST['store']))
 {
     $priority = $_POST['priority'];
     $name = $_POST['name'];
-    echo $priority, $name;
+    $qry = "INSERT INTO categories(priority,name) VALUES($priority,'$name')";
+    //execute query
+    include '../includes/dbconnection.php';
+    if(mysqli_query($conn,$qry))
+    {
+        echo '<script>alert("Category created successfully");
+            window.location.href = "category.php";
+        </script>';
+    }
+    else
+    {
+        echo "Cannot Create Category";
+    }
+    include '../includes/closeconnection.php';
 }
 
 ?>
--- admin/category.php
+++ admin/category.php
@@ -1,4 +1,9 @@
-<?php include 'header.php'; ?>
+<?php include 'header.php'; 
+include '../includes/dbconnection.php';
+$qry = "SELECT * FROM categories";
+$result = mysqli_query($conn,$qry);
+include '../includes/closeconnection.php';
+?>
 
 <h1 class="font-bold text-4xl">Categories</h1>
 <hr class="h-1 bg-blue-600">
@@ -13,13 +18,17 @@
         <th class="border p-2 bg-gray-100">Category Name</th>
         <th class="border p-2 bg-gray-100">Action</th>
     </tr>
+    <?php 
+    while($row = mysqli_fetch_assoc($result))
+    { ?>
     <tr class="text-center">
-        <td class="border p-2">1</td>
-        <td class="border p-2">News</td>
+        <td class="border p-2"><?php echo $row['priority'];?></td>
+        <td class="border p-2"><?php echo $row['name']; ?></td>
         <td class="border p-2">
             <a href="" class="bg-blue-600 text-white px-4 py-1 rounded-lg">Edit</a>
             <a href="" class="bg-red-600 text-white px-4 py-1 rounded-lg">Delete</a>
         </td>
     </tr>
+    <?php } ?>
 </table>
 <?php include 'footer.php'; ?>
