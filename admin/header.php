d6921233335553d1be1e4a8486211cb7d18dd82f
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-12-02 08:35:58 +0545

    updates

--- admin/header.php
+++ admin/header.php
@@ -3,6 +3,8 @@ session_start();
 if(!isset($_SESSION['check_login'])){
     header('location: ../login.php');
 }
+// check for base_url
+$page = basename($_SERVER['PHP_SELF']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,6 +13,13 @@ if(!isset($_SESSION['check_login'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SMC News Admin Panel</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <!-- For active url  -->
+    <style>
+        .active{
+            background-color: red;
+            color: white;
+        }
+    </style>
 </head>
 <body>
     <div class="flex">
@@ -18,16 +27,36 @@ if(!isset($_SESSION['check_login'])){
             <img src="https://smc.edu.np/wp-content/uploads/2023/11/smc-logo-circle.png" alt="" class="h-24 mt-5 mx-auto">
             <ul class="mt-5">
                 <li class="px-2">
-                    <a href="index.php" class="block hover:bg-gray-200 p-4 text-lg font-bold rounded-lg hover:text-xl">Dashboard</a>
+                    <a href="index.php" class="block hover:bg-red-500 p-4 text-lg font-bold rounded-lg hover:text-xl <?php 
+                    if($page == 'index.php')
+                    { 
+                        echo 'active';
+                    } 
+                    ?>">Dashboard</a>
                 </li>
                 <li class="px-2">
-                    <a href="category.php" class="block hover:bg-gray-200 p-4 text-lg font-bold rounded-lg hover:text-xl">Categories</a>
+                    <a href="category.php" class="block hover:bg-red-500 p-4 text-lg font-bold rounded-lg hover:text-xl <?php 
+                    if($page == 'category.php')
+                    { 
+                        echo 'active';
+                    } 
+                    ?>">Categories</a>
                 </li>
                 <li class="px-2">
-                    <a href="news.php" class="block hover:bg-gray-200 p-4 text-lg font-bold rounded-lg hover:text-xl">News</a>
+                    <a href="news.php" class="block hover:bg-red-500 p-4 text-lg font-bold rounded-lg hover:text-xl <?php 
+                    if($page == 'news.php')
+                    { 
+                        echo 'active';
+                    } 
+                    ?>">News</a>
                 </li>
                 <li class="px-2">
-                    <a href="notice.php" class="block hover:bg-gray-200 p-4 text-lg font-bold rounded-lg hover:text-xl">Notices</a>
+                    <a href="notice.php" class="block hover:bg-red-500 p-4 text-lg font-bold rounded-lg hover:text-xl <?php 
+                    if($page == 'notice.php')
+                    { 
+                        echo 'active';
+                    } 
+                    ?>">Notices</a>
                 </li>
                 <li class="px-2">
                     <a href="logout.php" class="block hover:bg-gray-200 p-4 text-lg font-bold rounded-lg hover:text-xl">Logout</a>
--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php include 'header.php'; 
 $qry = "SELECT * FROM news ORDER BY date DESC LIMIT 4";
+$qrynotice = "SELECT * FROM notices ORDER BY notice_date DESC";
 include 'includes/dbconnection.php';
 $resultnews = mysqli_query($conn, $qry);
+$resultnotice = mysqli_query($conn, $qrynotice);
 include 'includes/closeconnection.php';
 ?>
+
+
+<div class="px-12 flex mt-5">
+    <p class="bg-red-600 text-white px-10 py-5">Notices</p>
+    <marquee class="w-full bg-gray-100 p-5 text-lg font-bold">
+        <?php while($row = mysqli_fetch_assoc($resultnotice)){
+            echo $row['notice_title'];
+            echo " | ";
+         } ?>
+    </marquee>
+</div>
 
 <div class="px-12">
     <h2 class="font-bold my-5 py-2 px-2 border-l-4 border-blue-600 text-xl">Latest News</h2>
